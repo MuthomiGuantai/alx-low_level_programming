e461789a60e68b7ea424c1770cca173b26e8dcf2
MuthomiGuantai <muthomiguantai@gmail.com>
2022-03-11 16:27:53 +0300

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#include <math.h>
+#include <time.h>
+#include <stdlib.h>
 
-#define NUMBER 0
-#define LIMIT 9
+#define NUMBER '0'
+#define LIMIT '9'
 
 /**
  * main - single digits to base 10
@@ -13,10 +14,12 @@
 int main(void)
 {
 int numbers;
+
 for (numbers = NUMBER; numbers <= LIMIT; numbers++)
 {
 putchar(numbers);
 }
+
 putchar('\n');
 return (0);
 }
