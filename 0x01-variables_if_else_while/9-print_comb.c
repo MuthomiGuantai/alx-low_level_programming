21fd198ba444388fbe9f4300868de3b7bfeb291d
MuthomiGuantai <muthomiguantai@gmail.com>
2022-03-11 17:17:00 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+#define MIN 0
+#define SEP '0'
+
+/**
+ * main - print all possible combinations 
+ * of single-digit numbers.
+ * Return: return 0 if successful
+ */
+
+int main(void)
+{
+int number;
+for (number = MIN; number < MAX; number++)
+{
+putchar(number + SEP);
+if (number != (MAX - 1))
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
