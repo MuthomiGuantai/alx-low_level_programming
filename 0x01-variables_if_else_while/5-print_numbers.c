eec2947cf74f65f5f17d47862c37cfce0965660c
MuthomiGuantai <muthomiguantai@gmail.com>
2022-03-11 16:11:50 +0300

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,18 +3,20 @@
 
 #define NUMBER 0
 #define LIMIT 10
+
 /**
  * main - entry point
  *
  * Return: return 0 if successful
  */
+
 int main(void)
 {
-    int numbers;
-    for(numbers = NUMBER; numbers < LIMIT; numbers++)
-    {
-        printf("%d", numbers);
-    }
-    printf("\n");
-    return (0);
+int numbers;
+for(numbers = NUMBER; numbers < LIMIT; numbers++)
+{
+printf("%d", numbers);
+}
+printf("\n");
+return (0);
 }
