cbb71ee2cd1cdd0d3f15af4fafd669a55dd83f32
Bruce Muthomi <guantai.muthomi@imbank.co.ke>
2026-04-21 16:55:15 +0300

    Remove comments from print_diagsums function
    Removed function documentation for print_diagsums.

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,14 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * print_diagsums - prints the sum of the two diagonals of
- * a square matrix of integers
- *
- * @a: 2-dimensional array
- * @size: size of the matrix
- * Return: void
- */
 void print_diagsums(int *a, int size)
 {
 	int i = 0, len, diag_sum = 0;
